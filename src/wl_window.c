db8d44477846d1e78a572db5e76d9078857af680
Build Script <build@localhost>
2026-06-13 06:16:42 +0000

    patch: 0004-Fix-Window-size-on-unset-fullscreen.patch

--- src/wl_window.c
+++ src/wl_window.c
@@ -3037,6 +3037,8 @@ void _glfwSetWindowSizeWayland(_GLFWwindow* window, int width, int height)
             libdecor_state_free(frameState);
         }
 
+        _glfwInputWindowSize(window, width, height);
+
         if (window->wl.visible)
             _glfwInputWindowDamage(window);
     }
